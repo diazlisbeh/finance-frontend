04b61254904c8744167dac82ec5ba56ddaf1283b
Lisbeth Diaz <lizbeths.robles@gmail.com>
2023-05-23 20:08:32 -0400

    budget bug fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { MyContext } from '@/context/context'
 import { useRouter } from 'next/router' 
 
@@ -12,10 +12,14 @@ export default function inde() {
   const router = useRouter();
 const {login}  = useContext(MyContext);
 
+useEffect(()=>{
+
+router.push('/Login')
+})
+
+
   return (
     <>
-    <p>Vamo a ver klk</p>
-    <button onClick={() => router.push('/Login')}>dale bro</button>
     </>
   )
 }
